0d960fde33789c500552346edc0a2c9b4d894e78
Peter Dell <peter.dell@web.de>
2025-03-08 12:02:08 +0100

    Update Makefile.bat
    Use environment variables, add quoting and conditional compilation

--- src/Makefile.bat
+++ src/Makefile.bat
@@ -1,24 +1,30 @@
 @echo off
 setlocal
-set PATH=C:\jac\system\Java\Programming\Repositories\wudsn-ide-tools\PAS\FPC.jac;%PATH%
-set MP_FOLDER=C:\jac\system\Atari800\Programming\Repositories\Mad-Pascal\
+set PATH=%WUDSN_TOOLS_FOLDER%\PAS\FPC.jac;%PATH%
+set MP_FOLDER="%~dp0"\..
 set MP_SRC_FOLDER=%MP_FOLDER%\src
-set MP_EXE=%MP_SRC_FOLDER%\mp.exe
+
 set TEST_EXE=%MP_SRC_FOLDER%\Test-0.exe
+set MP_EXE=%MP_SRC_FOLDER%\mp.exe
 
 cd /d %MP_SRC_FOLDER%
 
-call fpc.bat %MP_SRC_FOLDER%\Test-0.pas
-if errorlevel 1 goto :eof
-if exist %TEST_EXE% (
-  echo Starting test program %TEST_EXE%.
-   %TEST_EXE%
+if not "%TEST_EXE%"=="" (
+  if exist "%TEST_EXE%" del "%TEST_EXE%"
+  call fpc.bat %MP_SRC_FOLDER%\Test-0.pas
+  if errorlevel 1 goto :eof
+  if exist "%TEST_EXE%" (
+     echo Starting test program "%TEST_EXE%".
+     %TEST_EXE%
+  )
 )
 
-rem goto :eof
-
-if exist %MP_EXE% del %MP_EXE%
-call fpc.bat %MP_SRC_FOLDER%\mp.pas
-if errorlevel 1 goto :eof
-
-if exist %MP_EXE%  %MP_EXE% -ipath:%MP_FOLDER%\lib Test-MP.pas
+if not "%MP_EXE%"=="" (
+  if exist "%MP_EXE%" del "%MP_EXE%"
+  call fpc.bat %MP_SRC_FOLDER%\mp.pas
+  if errorlevel 1 goto :eof
+  if exist "%MP_EXE%" (
+    echo Starting test program %MP_EXE%.
+    if exist %MP_EXE%  %MP_EXE% -ipath:%MP_FOLDER%\lib Test-MP.pas
+  )
+)
