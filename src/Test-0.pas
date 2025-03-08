159ffacf54683b3f4b182082d278d4fe9a3c7346
Peter Dell <peter.dell@web.de>
2025-03-08 15:46:50 +0100

    Extended Tests

--- src/Test-0.pas
+++ src/Test-0.pas
@@ -2,44 +2,63 @@ program test;
 
 {$i define.inc}
 
-uses Console, Common, FileIO
+uses Console, Common, FileIO, Scanner, Utilities
 {$IFDEF PAS2JS}
      ,browserconsole
-{$ENDIF} 
+{$ENDIF}
 ;
 
-const filePath = 'Test-MP.pas';
+procedure StartTest(name: string);
+begin
+  WriteLn('Unit Test '+name+' started.');
+end;
+
+procedure FailTest(msg: string);
+begin
+  WriteLn('ERROR: '+msg);
+end;
+
+
+procedure EndTest(name: string);
+begin
+  WriteLn('Unit Test '+name+' ended.');
+end;
+
 
 procedure TestNative(filePath: TFilePath);
-var 
+var
   f: TextFile;
   s: string;
 begin
+  StartTest('TestFileNative');
+
   AssignFile(f, filePath);
 
   try
     reset(f); // Open the file for reading
     readln(f, s);
-    writeln('Text read from file: ', s) 
-   
+    writeln('Text read from file: ', s)
+
   finally
     CloseFile(f);
   end;
-  Writeln('TestFileIO completed.');
+  EndTest('TestFileNative');
 end;
 
 procedure TestFileIO(filePath: TFilePath);
 var binFile: IBinaryFile;
 var c: Char;
 begin
+  StartTest('TestFileIO');
 
   binFile:=TFileSystem.CreateBinaryFile;
   binFile.Assign(filePath);
   try
     binFile.Reset;
-    
+
     while not binFile.EOF do
     begin
+      c:=' ';
       binFile.Read(c);
       Write(c);
       // WriteLn(IntToStr(Ord(c)));
@@ -47,19 +66,53 @@ begin
 //      binFile.Read(c);
     binFile.Close;
   except
-    Writeln('ERROR: TestFileIO failed with Exception.');
-  	
+    FailTest('Failed with Exception.');
+
   end;
-  binFile.Free;
-  Writeln('TestFileIO completed.');
+  EndTest('TestFileIO');
 end;
 
+procedure TestUnitFile;
+const TEST_MP_FILE_PATH = 'Test-MP.pas';
 begin
-  TestNative(filePath);
-  TestFileIO(filePath);
- 
+  StartTest('TestUnitFile');
+  TestNative(TEST_MP_FILE_PATH);
+  TestFileIO(TEST_MP_FILE_PATH);
+  EndTest('TestUnitFile');
+end;
+
+procedure TestUnitCommon;
+var filePath: String;
+begin;
+  StartTest('TestUnitCommon');
+
+  // Unit Scanner
+  Program_NAME:='TestProgram';
+  NumTok:=0;
+  // Kind, UnitIndex, Line, Column, Value
+  AddToken(PROGRAMTOK, 1,1,1,0);
+
   // Unit Common
-  SetLength(UnitPath,2);
+  UnitPath:=nil;
+  SetLength(UnitPath,1);
   UnitPath[0]:='lib';
-  // FindFile('TestUnit', 'unit');
+  filePath:='';
+  try
+  filePath:=FindFile('TestUnit', 'unit');
+  except on  ex: THaltException do
+    begin
+       Assert(ex.GetExitCode = THaltException.COMPILING_ABORTED);
+    end;
+  end;
+  Assert(filePath<>'', 'Non-existing TestUnit found');
+
+  EndTest('TestUnitCommon');
+end;
+
+
+begin
+  TestUnitFile;
+  TestUnitCommon;
+
+  Writeln('Main completed.');
 end.
