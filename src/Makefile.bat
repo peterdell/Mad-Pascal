34835f178eb26e8c3cfa55a54f8872ea3ed3dced
Peter Dell <peter.dell@web.de>
2025-03-09 15:22:40 +0100

    Add float tests and binary file compare

--- src/Makefile.bat
+++ src/Makefile.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 setlocal
 set PATH=%WUDSN_TOOLS_FOLDER%\PAS\FPC.jac;%WUDSN_TOOLS_FOLDER%\ASM\MADS\bin\windows_x86_64;%PATH%
 set MP_FOLDER=%~dp0..
@@ -21,41 +21,34 @@ if not "%TEST_EXE%"=="" (
   )
 )
 
-set TEST_1_FOLDER=%MP_SRC_FOLDER%
-set TEST_1_FILE=Test-MP
-
-set TEST_2_FOLDER=C:\jac\system\Atari800\Programming\Repositories\Mad-Pascal\samples\a8\games\PacMad
-set TEST_2_FILE=pacmad
+if not "%MP_EXE%"=="" (
 
 rem Regression test with standard MP.
-if 1==1 (
-if not "%MP_EXE%"=="" (
-  echo.
-  echo INFO: Compiling with WUDSN version.
-  echo ===================================
-  echo.
-  call :run_mp %WUDSN_MP_EXE% %TEST_1_FOLDER% %TEST_1_FILE%
-  call :run_mp %WUDSN_MP_EXE% %TEST_2_FOLDER% %TEST_2_FILE%
-
-   
-  echo.
-  echo.
-  echo INFO: Comiling with new version.
-  echo ================================
-  echo.
-  if exist "%MP_EXE%" del "%MP_EXE%"
-  call fpc.bat %MP_SRC_FOLDER%\mp.pas
-  if errorlevel 1 goto :eof
-  if exist "%MP_EXE%" (
-    call :run_mp %MP_EXE% %TEST_1_FOLDER% %TEST_1_FILE%
-    call :run_mp %MP_EXE% %TEST_2_FOLDER% %TEST_2_FILE%
-  )
-)
-)
+   if 1==0 (
+     echo.
+     echo INFO: Compiling with WUDSN version.
+     echo ===================================
+     echo.
+     call :run_tests %WUDSN_MP_EXE%
+   )
+ 
+    
+   echo.
+   echo.
+   echo INFO: Comiling with new version.
+   echo ================================
+   echo.
+   if exist "%MP_EXE%" del "%MP_EXE%"
+   call fpc.bat %MP_SRC_FOLDER%\mp.pas
+   if errorlevel 1 goto :eof
+   if exist "%MP_EXE%" (
+     call :run_tests %MP_EXE%
+   )
+ )
 
 goto :eof
 
-
+ rem IN: %1=Path to mp.exe´, %2=Folder of test source %3=File name without file extension of the ".pas" file
 :run_mp
   set MP=%1
   set TEST_FOLDER=%2
@@ -63,12 +56,13 @@ goto :eof
   
   set MP_INPUT_PAS=%TEST_MP%.pas
   set MP_OUTPUT_ASM=%TEST_MP%.a65
+  set MADS_OUTPUT_XEX_REF=%TEST_MP%-Reference.xex
+  echo %MADS_OUTPUT_XEX_REF%
   if %MP%==%WUDSN_MP_EXE% (
-    set MADS_OUTPUT_XEX=%TEST_MP%-WUDSN.xex
+    set MADS_OUTPUT_XEX=%MADS_OUTPUT_XEX_REF%
   ) else (
     set MADS_OUTPUT_XEX=%TEST_MP%.xex
   )
-  
   pushd %TEST_FOLDER%
   echo INFO: Compiling "%MP_INPUT_PAS%" in "%TEST_FOLDER%" with "%MP%".
   if exist %MP_OUTPUT_ASM% del %MP_OUTPUT_ASM%
@@ -79,7 +73,7 @@ goto :eof
      mads %MP_OUTPUT_ASM% -x -i:%MP_FOLDER%\base -o:%MADS_OUTPUT_XEX%
      if exist %MADS_OUTPUT_XEX% (
        echo Starting test program "%MADS_OUTPUT_XEX%".
-       %MADS_OUTPUT_XEX%
+       rem %MADS_OUTPUT_XEX%
      ) else (
        echo ERROR: MADS output file %MADS_OUTPUT_XEX% not created.
        pause
@@ -87,6 +81,19 @@ goto :eof
   ) else (
     echo ERROR: MP output file %MP_OUTPUT_ASM% not created.
     pause
+  )
+  
+  REM TODO Compare file if both files reference exists
+  if not "%MADS_OUTPUT_XEX%" == "%MADS_OUTPUT_XEX_REF%" (
+    if exist %MADS_OUTPUT_XEX% (
+
+      if exist %MADS_OUTPUT_XEX_REF% (
+        fc /b %MADS_OUTPUT_XEX%  %MADS_OUTPUT_XEX_REF%
+        if errorlevel 1 goto :fc_error
+      ) else (
+        echo WARNING: Reference file  %MADS_OUTPUT_XEX_REF% does not exist, no comparsion possible.
+      )
+    )
   )
   popd
 goto :eof
@@ -96,4 +103,26 @@ goto :eof
   echo ERROR: Mad-Pascal error.
   pause
   goto :eof
+ 
 
+:fc_error
+  popd
+  echo ERROR: %MADS_OUTPUT_XEX%  and %MADS_OUTPUT_XEX_REF% are binary different.
+  pause
+  goto :eof
+
+ rem Run all tests with a given mp.exe.
+ rem IN: Path to mp.exe
+ rem
+:run_tests
+rem    call :run_mp % %MP_SRC_FOLDER% Test-MPP
+rem    call :run_mp %1=%MP_FOLDER%\samples\a8\games\PacMad pacmadd
+    call :run_mp %1 %MP_FOLDER%\samples\a8\graph_crossplatform fedorahat
+    call :run_mp %1 %MP_FOLDER%\samples\a8\graph_crossplatform cannabis
+    call :run_mp %1 %MP_FOLDER%\samples\a8\graph_crossplatform snowflake
+rem    call :run_mp %1 %MP_FOLDER%\samples\a8\graph_crossplatform splineCompiling spline.pas
+rem ERROR: spline.pas (98,54) Error: Can't determine which overloaded function 'LINE' to call
+
+    call :run_mp %1 %MP_FOLDER%\samples\a8\graph_crossplatform fern
+    goto :eof
+ 
